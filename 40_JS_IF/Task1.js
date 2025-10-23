026bc37db05876b779340e3258dc628dacf053f5
Lennart <lennart.roeck@gmail.com>
2025-10-23 12:55:35 +0200

    if finished task 1 then commit

---
+++ 40_JS_IF/Task1.js
@@ -0,0 +1,11 @@
+let hehehea = 67;
+
+if (hehehea > 0) {
+    console.log(" Die Nummer ist Positiv");
+}
+if (hehehea < 0) {
+    console.log("Die Nummer ist Negativ");
+}
+if (hehehea == 0) {
+    console.log("Die Nummer ist 0");
+}
