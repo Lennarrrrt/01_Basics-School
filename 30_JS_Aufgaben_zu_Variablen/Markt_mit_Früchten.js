afe45e913894747040514cd9b744b40844f48645
Lennart <lennart.roeck@gmail.com>
2025-10-23 12:41:24 +0200

    finished markt

---
+++ 30_JS_Aufgaben_zu_Variablen/Markt_mit_Früchten.js
@@ -0,0 +1,10 @@
+let banane = 17;
+let apfel = 8;
+let bananePreisprokilo = 2.14;//in Euro
+let apfelPreisproKilo = 3.43;//in Euro
+let apfelgewicht = 0.34;//in kg
+let bananengewicht = 0.22;//in kg
+let gesamtpreisbanane = bananePreisprokilo * bananengewicht * banane;
+let gesamtpreisapfel = apfelPreisproKilo * apfelgewicht * apfel;
+console.log (gesamtpreisapfel)
+console.log (gesamtpreisbanane)
