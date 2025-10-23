2fca5381a10ea06c05c6b2c4c3efacf827d980fd
Lennart <lennart.roeck@gmail.com>
2025-10-23 12:44:04 +0200

    gewicht preis erstellt

---
+++ 30_JS_Aufgaben_zu_Variablen/GewichtPreis.js
@@ -0,0 +1,9 @@
+
+let bananePreisprokilo = 2.14;//in Euro
+let apfelPreisproKilo = 3.43;//in Euro
+let apfelgewicht = 1000;//in kg
+let bananengewicht = 1000;//in kg
+let gesamtpreisbanane = bananePreisprokilo * bananengewicht ;
+let gesamtpreisapfel = apfelPreisproKilo * apfelgewicht ;
+console.log (gesamtpreisapfel)
+console.log (gesamtpreisbanane)
