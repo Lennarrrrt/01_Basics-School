4467ede3fe3f3f7315554c1fb0fc7fcd260ca747
Lennart <lennart.roeck@gmail.com>
2025-10-23 13:00:53 +0200

    if finisched task to you can commit

---
+++ 40_JS_IF/Task2.js
@@ -0,0 +1,12 @@
+
+let ("trafficlight") = "green";
+if (trafficlight === "green") {
+    console.log("You can go");
+}
+if (trafficlight === "yellow") 
+{
+    console.log("Caution");
+}
+if (trafficlight === "red") {
+    console.log("Stop!");
+}
