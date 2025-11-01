8165b382c32790d7a98774c10fb8a89982271ef8
Lennart <lennart.roeck@gmail.com>
2025-11-01 20:41:57 +0100

    did some stuff

---
+++ 41_JS_If.then.else/Task1.js
@@ -0,0 +1,17 @@
+let number = -2;
+if (number > 0) {
+    console.log("Die Nummer ist Positiv");
+} else {
+    console.log("Die Nummer ist nicht positiv");
+}
+
+if (number < 0) {
+    console.log("Die Nummer ist Negativ");
+} else {
+    console.log("Die Nummer ist nicht negativ");
+}
+if (number == 0) {
+    console.log("Die Nummer ist 0");
+} else {
+    console.log("Die Nummer ist nicht 0");
+}
---
+++ 42_JS_If.else.random/Task1.js
@@ -0,0 +1,13 @@
+let randomnumber = Math.floor(Math.random() * 10);
+
+if (randomnumber > 5) {
+    console.log("The number is greater than 5")
+};
+
+if (randomnumber <= 5) {
+    console.log("The number is less than 5")
+};
+
+if (randomnumber === 5) {
+    console.log("The number is exactly 5")
+}
