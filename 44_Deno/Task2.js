ff9f28ac49152c42dc76957cd1a2a70123a0b466
Lennart <lennart.roeck@gmail.com>
2025-11-06 19:31:06 +0100

    44/task2

---
+++ 44_Deno/Task2.js
@@ -0,0 +1,17 @@
+let number = prompt("Bitte eine Nummer eingeben:");
+if (number > 0) {
+    console.log("Die Nummer ist Positiv");
+} else {
+    console.log("Die Nummer ist nicht positiv");
+}
+
+if (number < 0) {
+    console.log("Die Nummer ist Negativ");
+} else {
+    console.log("Die Nummer ist nicht negativ");
+}
+if (number == 0) {
+    console.log("Die Nummer ist 0");
+} else {
+    console.log("Die Nummer ist nicht 0");
+}
