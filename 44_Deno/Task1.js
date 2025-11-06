e25bbf789ac3d143030fc1095f1d1c68a705af3a
Lennart <lennart.roeck@gmail.com>
2025-11-06 19:26:29 +0100

    44/task1 finished

---
+++ 44_Deno/Task1.js
@@ -0,0 +1,8 @@
+let password = prompt("Please enter your password:");
+
+if (password === "password") {
+        console.log("Access granted.");
+    
+} else {
+    console.log("Access denied.");
+}
