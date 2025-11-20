c0172e861b0ff0122e14d8dee65ae54010c7fa76
Lennart <lennart.roeck@gmail.com>
2025-11-20 17:17:18 +0100

    11/task2

--- 11_Arrays/Task2.js
+++ 11_Arrays/Task2.js
@@ -1,4 +1,4 @@
-let arr = ["Susi", "Paula", "Hans", "Susi", "Paula", "Hans"];
+let arr = ["Susi", "Paula", "Hans"];
 
 
 let names = "";
@@ -10,5 +10,17 @@ for (let i = 0; i < arr.length; i++) {
     else
         names += arr[i] + ", ";
 }
+console.log(names);
+console.log("Sepp kommt dazu.");
+arr.push("Sepp");
 
+names = "";
+for (let i = 0; i < arr.length; i++) {
+    if (i === arr.length - 2) {
+        names += arr[i] + " " + "und" + " ";
+    } else if (i === arr.length - 1)
+        names += arr[i];
+    else
+        names += arr[i] + ", ";
+}
 console.log(names);
