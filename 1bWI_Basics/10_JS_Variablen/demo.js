64ea685122011913891dfe19e213314ffde6d11f
Lennart <lennart.roeck@gmail.com>
2026-02-19 13:07:06 +0100

    zzu

--- 1bWI_Basics/10_JS_Variablen/demo.js
+++ 1bWI_Basics/10_JS_Variablen/demo.js
@@ -3,4 +3,9 @@ let b = 8
 let c = 10
 
 let result = (a+b+c)/4
+console.log (result);let a = 7
+let b = 8
+let c = 10
+
+let result = (a+b+c)/4
 console.log (result);
--- 1bWI_Basics/20_JS_Strings&Operatoren/Result.js
+++ 1bWI_Basics/20_JS_Strings&Operatoren/Result.js
@@ -1,3 +1,6 @@
 sum = 17.4 / 3.9;
 console.log("The result of 17.4 divided by 3.9 is " + sum);
 
+sum = 17.4 / 3.9;
+console.log("The result of 17.4 divided by 3.9 is " + sum);
+
